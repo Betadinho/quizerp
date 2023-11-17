6199f26ec9dadb45f5bb034888571cca8409b05e
alphamario.prechtl <prechtl@billiger-mietwagen.de>
2023-11-17 15:04:16 +0100

    add create and read savefile

--- app.py
+++ app.py
@@ -1,3 +1,27 @@
 import eel
-eel.init('web')
-eel.start('templates/main.html', jinja_templates='templates')
+import constants
+from pathlib import Path
+import json
+#eel.init('web')
+#eel.start(constants.MAIN_PAGE_DIR, jinja_templates=constants.JINJA_TEMPLATE_DIR)
+
+SAVE_FILE_PATH = Path(constants.SAVE_FILE_DIR) / constants.SAVE_FILE_NAME
+
+def create_save_file():
+  with open(SAVE_FILE_PATH, 'w') as f:
+    content = {'quizlist' : {}}
+    json.dump(content, f, indent=2)
+             
+def read_save_file():
+  with open(SAVE_FILE_PATH, 'r') as f:
+    result = json.load(f)
+  return result
+
+def init_data():
+  if Path(SAVE_FILE_PATH).exists() == False:
+    create_save_file()
+
+  data = read_save_file()
+  return data
+
+data = init_data()
